761f121402334cb9863cca9588199c5b01d3afb3
Charles <charlesso1917@gmail.com>
2026-06-06 10:39:59 +0800

    Module 1 project - hello me

---
+++ hello_me.py
@@ -0,0 +1,24 @@
+# Requirements 
+# Use print() to display your name, age, nationality, and a fun fact about yourself
+# Display a simple ASCII art design using print statements
+# Print a short menu of your interests in a clean formatted layout
+# Program runs cleanly from start to finish with no errors
+
+print("      [-_-] 67   ")
+print("     /|   |/   ")
+print("")
+print("=" * 18)
+print("     About Me")
+print("Name: CLASSIFIED") 
+print("Age: 18")
+print("Nationality: Filipino")
+print("=" * 18)
+print("")
+print("     Interests:") 
+print("  Technology: Programming, in general!")
+print("  Sports: Table Tennis")
+print("  Philosophy")
+print("  Cuisines")
+print("  Law Enforcement Games")
+print("")
+print("=" * 18)
